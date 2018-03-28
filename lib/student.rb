0418812933d62eff9658093a5a489f8d32ae3f33
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-28 02:34:34 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,9 +50,8 @@ class Student
   end
 
   def self.new_from_db(row)
-    self.new(row[0], row[1], row[2])
-
-
+    new_student = self.new(row[0], row[1], row[2])
+    new_student
   end
 
   def self.find_by_name(name)
