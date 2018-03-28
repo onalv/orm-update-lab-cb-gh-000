b146f51dbe4fe3bf882b3b17e463b6512eb0312b
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-28 02:29:56 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,11 +50,18 @@ class Student
   end
 
   def self.new_from_db(row)
-
+    
   end
 
-  def self.find_by_name
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
 
+    DB[:conn].execute(sql, name)
   end
 
   def update
