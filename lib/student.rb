5d69ac6ee4bfe15192ed1d61152fc202f3532503
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-28 02:26:49 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,8 +44,9 @@ class Student
     end
   end
 
-  def self.create
-
+  def self.create(name, grade)
+    new_student = self.new(name, grade)
+    new_student.save
   end
 
   def self.new_from_db(row)
